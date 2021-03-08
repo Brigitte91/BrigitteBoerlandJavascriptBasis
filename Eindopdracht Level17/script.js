2bb80624829d0143cbf232bbd6a4a86c91643bee
Brigitte91 <71765969+Brigitte91@users.noreply.github.com>
2021-03-09 00:34:40 +0100

    Add files via upload

--- Eindopdracht Level17/script.js
+++ Eindopdracht Level17/script.js
@@ -1,4 +1,33 @@
-/* const menu = document.querySelector("#togglemenu");
-const toggleMenu = () => menu.toggleAttribute("menulist");
+const red = document.getElementById("red");
+const orange = document.getElementById("orange");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+const aqua = document.getElementById("aqua");
+const blue = document.getElementById("blue");
+const indigo = document.getElementById("indigo");
+const violet = document.getElementById("violet");
+const body = document.getElementsByTagName("body");
 
-menu.addEventListener("onclick", toggleMenu); */
+const functionRed = function(color){
+
+}
+
+console.log(red);
+console.log(orange);
+
+/*
+Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+  
+      If there's no active class
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+  
+      Add the active class to the current/clicked button
+      this.className += " active";
+    });
+  }
+ */  
