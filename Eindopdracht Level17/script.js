a86b9a34476f0c5f6bc2f9bdcb7129fb1b29d9bd
Brigitte91 <71765969+Brigitte91@users.noreply.github.com>
2021-03-09 16:41:29 +0100

    Add files via upload

--- Eindopdracht Level17/script.js
+++ Eindopdracht Level17/script.js
@@ -8,26 +8,14 @@ const indigo = document.getElementById("indigo");
 const violet = document.getElementById("violet");
 const body = document.getElementsByTagName("body");
 
-const functionRed = function(color){
-
+const changeColor = function(color){
+    Element.classList.add("color");
 }
+
+let color = // kleur die bij de radiobutton hoort
 
 console.log(red);
 console.log(orange);
 
-/*
-Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-  
-      If there's no active class
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-      }
-  
-      Add the active class to the current/clicked button
-      this.className += " active";
-    });
-  }
- */  
+
+
